a12e77e05f250d4b2fc992c271f864be0bc60bb4
owentotten <146154955+owentotten@users.noreply.github.com>
2025-09-04 10:49:19 -0400

    Practice 2

---
+++ Practice/Practice2/Practice2/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Owen Totten
+ * *Tuesday 9/2/25
+ * Practice with user imputs
+ */
+
+
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Practice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dblVal1, dblVal2;
+            Console.WriteLine("Give me the first value: ");
+            dblVal1 = Convert.ToDouble(Console.ReadLine());
+            //Test
+            //Console.WriteLine(dblVal1);
+
+            Console.WriteLine("Give me the second value: ");
+            dblVal2 = Convert.ToDouble(Console.ReadLine());
+            // Test
+            double dblTotal = dblVal1 + dblVal2;
+            Console.WriteLine($"The sum of {dblVal1} and {dblVal2} is {dblVal2}");
+
+            
+            int dblVal1, dblVal2;
+            Console.WriteLine("Give me the first value: ");
+            dblVal1 = Convert.ToDouble(Console.ReadLine());
+            //Test
+            //Console.WriteLine(dblVal1);
+
+            Console.WriteLine("Give me the second value: ");
+            dblVal2 = Convert.ToDouble(Console.ReadLine());
+            // Test
+            double dblTotal = dblVal1 + dblVal2;
+            Console.WriteLine($"The sum of {dblVal1} and {dblVal2} is {dblVal2}");
+
+        }
+
+    }
+}
